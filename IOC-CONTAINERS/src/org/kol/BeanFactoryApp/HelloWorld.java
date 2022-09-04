aa339fafa791344b53694792a75dd6150cf0e6b1
iSemicolon <palash.samanta7497@gmail.com>
2022-09-04 11:46:11 +0530

    IOC Container: BeanFactory

---
+++ IOC-CONTAINERS/src/org/kol/BeanFactoryApp/HelloWorld.java
@@ -0,0 +1,17 @@
+package org.kol.BeanFactoryApp;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+
+		System.out.println("HelloWorld Object Created");
+	}
+
+	private String str="Welcome In Spring";
+
+	void display()
+	{
+		System.out.println("Hello Spring Fan "+str);
+	}
+
+}
---
+++ IOC-CONTAINERS/src/org/kol/BeanFactoryApp/Test.java
@@ -0,0 +1,18 @@
+package org.kol.BeanFactoryApp;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Resource res=new ClassPathResource("Spring.xml");;
+		BeanFactory bef=new XmlBeanFactory(res);
+
+		HelloWorld obj=(HelloWorld) bef.getBean("world");
+		obj.display();
+	}
+
+}
